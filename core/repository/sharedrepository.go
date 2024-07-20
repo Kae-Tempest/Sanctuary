041328a325b933be9dc212d458f5822dbaeb5f8f
Kae-Tempest <katsuhiko.tempest@gmail.com>
2024-07-20 08:59:45 +0200

    [WIP] Add GetPlayerByEmail and GetCreatureByName

--- core/repository/sharedrepository.go
+++ core/repository/sharedrepository.go
@@ -17,9 +17,28 @@ func GetPlayerById(ctx context.Context, db *pgxpool.Pool, playerID string) (enti
 	return player, nil
 }
 
+func GetPlayerByEmail(ctx context.Context, db *pgxpool.Pool, playerEmail string) (entities.Player, error) {
+	var player entities.Player
+	err := pgxscan.Get(ctx, db, &player, `SELECT * FROM players where email = $1`, playerEmail)
+	if err != nil {
+		return player, err
+	}
+
+	return player, nil
+}
+
 func GetCreatureById(ctx context.Context, db *pgxpool.Pool, creatureID string) (entities.Creatures, error) {
 	var creature entities.Creatures
 	err := pgxscan.Get(ctx, db, &creature, `SELECT * FROM creatures where id = $1`, creatureID)
+	if err != nil {
+		return creature, err
+	}
+	return creature, nil
+}
+
+func GetCreatureByName(ctx context.Context, db *pgxpool.Pool, creatureName string) (entities.Creatures, error) {
+	var creature entities.Creatures
+	err := pgxscan.Get(ctx, db, &creature, `SELECT * FROM creatures where name = $1`, creatureName)
 	if err != nil {
 		return creature, err
 	}
