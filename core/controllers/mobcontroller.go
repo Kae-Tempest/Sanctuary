53bba20d1c787e11061df75a60b18d487e0fcede
Kae-Tempest <katsuhiko.tempest@gmail.com>
2024-07-23 10:18:32 +0200

    [WIP] Refacto BDD / Entities | Refacto Creature -> Mob

---
+++ core/controllers/mobcontroller.go
@@ -0,0 +1,443 @@
+package controllers
+
+import (
+	"errors"
+	"github.com/jackc/pgx/v5"
+	"net/http"
+	"sanctuary-api/database"
+	"sanctuary-api/entities"
+	"sanctuary-api/repository"
+	"strconv"
+
+	"github.com/georgysavva/scany/v2/pgxscan"
+	"github.com/gin-gonic/gin"
+)
+
+// GET \\
+
+func GetAllMobs(c *gin.Context) {
+	db := database.Connect()
+	var mobs []entities.Mob
+	err := pgxscan.Select(ctx, db, &mobs, `SELECT * FROM mobs`)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+	if len(mobs) > 0 {
+		c.JSON(http.StatusOK, &mobs)
+	} else {
+		c.JSON(http.StatusNotFound, gin.H{})
+	}
+	c.Done()
+}
+func GetOneMob(c *gin.Context) {
+	db := database.Connect()
+	id := c.Param("id")
+
+	mob, err := repository.GetCreatureByID(ctx, db, id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	c.JSON(http.StatusOK, &mob)
+	c.Done()
+}
+func GetMobSpawn(c *gin.Context) {
+	db := database.Connect()
+	id := c.Param("id")
+
+	mob, err := repository.GetCreatureByID(ctx, db, id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	var creatureSpawn entities.MobSpawns
+	err = pgxscan.Get(ctx, db, &creatureSpawn, `SELECT * FROM mob_spawn where mob_id = $1`, mob.ID)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	c.JSON(http.StatusOK, &creatureSpawn)
+	c.Done()
+}
+func GetMobSkill(c *gin.Context) {
+	db := database.Connect()
+	id := c.Param("id")
+
+	mob, err := repository.GetCreatureByID(ctx, db, id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	var creatureSkill []entities.MobSkill
+	err = pgxscan.Select(ctx, db, &creatureSkill, `SELECT * FROM mob_skill where mob_id = $1`, mob.ID)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	c.JSON(http.StatusOK, &creatureSkill)
+	c.Done()
+}
+
+// POST \\
+
+func CreateMob(c *gin.Context) {
+	db := database.Connect()
+	var mobForm entities.Mob
+	if err := c.ShouldBindBodyWithJSON(&mobForm); err != nil {
+		c.String(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	// check if creature exist
+	existingMob, ecErr := repository.GetCreatureByName(ctx, db, mobForm.Name)
+	if errors.Is(ecErr, pgx.ErrNoRows) {
+		c.JSON(http.StatusConflict, &existingMob)
+	}
+	if ecErr != nil && !errors.Is(ecErr, pgx.ErrNoRows) {
+		c.String(http.StatusBadRequest, "bad request")
+	}
+	// insert creature
+	_, err := db.Exec(ctx, `INSERT INTO mobs (name, is_pet, strength, constitution, mana, stamina, dexterity, intelligence, wisdom, charisma, level, hp) values ($1, $2, $3, $4,$5, $6, $7, $8,$9, $10, $11, $12)`,
+		mobForm.Name, mobForm.IsPet, mobForm.Strength, mobForm.Constitution, mobForm.Mana, mobForm.Stamina,
+		mobForm.Dexterity, mobForm.Intelligence, mobForm.Wisdom, mobForm.Charisma, mobForm.Level, mobForm.HP)
+	if err != nil {
+		c.String(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	// Get Creature
+	mob, cErr := repository.GetCreatureByName(ctx, db, mobForm.Name)
+	if cErr != nil {
+		c.String(http.StatusBadRequest, "bad request")
+	}
+	// return Creature
+	c.JSON(http.StatusCreated, &mob)
+	c.Done()
+
+}
+func AddMobSpawn(c *gin.Context) {
+	db := database.Connect()
+	id := c.Param("id")
+	var mobSpawnForm entities.MobSpawns
+	if err := c.ShouldBindBodyWithJSON(&mobSpawnForm); err != nil {
+		c.String(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	mob, err := repository.GetCreatureByID(ctx, db, id)
+	if err != nil {
+		c.String(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	var selectedEmplacement entities.Locations
+	err = pgxscan.Get(ctx, db, &selectedEmplacement, `SELECT id FROM locations where id = $1`, mobSpawnForm.LocationID)
+	if err != nil {
+		c.String(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	_, err = db.Exec(ctx, `INSERT INTO mob_spawn (mob_id, location_id, level_required, spawn_rate) values ($1, $2, $3, $4)`,
+		mob.ID, selectedEmplacement.ID, mobSpawnForm.LevelRequired, mobSpawnForm.SpawnRate)
+	if err != nil {
+		c.String(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	var mobSpawn entities.MobSpawns
+	err = pgxscan.Get(ctx, db, &mobSpawn, `SELECT * FROM mob_spawn where mob_id = $1 AND location_id = $2`, mob.ID, selectedEmplacement.ID)
+	if err != nil {
+		c.String(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	c.JSON(http.StatusCreated, &mobSpawn)
+	c.Done()
+
+}
+func AddMobSkill(c *gin.Context) {
+	db := database.Connect()
+	id := c.Param("id")
+	var mobSkillForm entities.MobSkill
+	if err := c.ShouldBindBodyWithJSON(&mobSkillForm); err != nil {
+		c.String(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	mob, err := repository.GetCreatureByID(ctx, db, id)
+	if err != nil {
+		c.String(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	var seletecdSkill entities.Locations
+	err = pgxscan.Get(ctx, db, &seletecdSkill, `SELECT id FROM skills where id = $1`, mobSkillForm.SkillID)
+	if err != nil {
+		c.String(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	_, err = db.Exec(ctx, `INSERT INTO mob_skill (mob_id, skill_id) values ($1,$2)`, id, mobSkillForm.SkillID)
+	if err != nil {
+		c.String(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	var mobSkill entities.MobSpawns
+	err = pgxscan.Get(ctx, db, &mobSkill, `SELECT * FROM skills s join mob_skill cs on s.id = cs.skill_id where mob_id = $1 and s.id = $2`, mob.ID, seletecdSkill.ID)
+	if err != nil {
+		c.String(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	c.JSON(http.StatusCreated, &mobSkill)
+	c.Done()
+}
+func AddMobLoot(c *gin.Context) {}
+
+// PATCH \\
+
+func UpdateMob(c *gin.Context) {
+	db := database.Connect()
+	id := c.Param("id")
+
+	var mobUpdateForm entities.Mob
+	if err := c.ShouldBindBodyWithJSON(&mobUpdateForm); err != nil {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	mob, err := repository.GetCreatureByID(ctx, db, id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	_, err = db.Exec(ctx, `UPDATE creatures set (name, is_pet, strength, constitution, mana, stamina, dexterity, intelligence, wisdom, charisma, level, hp ) 
+    = ($2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13) where id = $1`,
+		mob.ID, mobUpdateForm.Name, mobUpdateForm.IsPet, mobUpdateForm.Strength, mobUpdateForm.Mana, mobUpdateForm.Stamina,
+		mobUpdateForm.Dexterity, mobUpdateForm.Intelligence, mobUpdateForm.Wisdom, mobUpdateForm.Level, mobUpdateForm.HP)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	mob, err = repository.GetCreatureByID(ctx, db, id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	c.JSON(http.StatusOK, &mob)
+	c.Done()
+
+}
+func UpdateMobSpawn(c *gin.Context) {
+	db := database.Connect()
+	id := c.Param("id")
+
+	var mobSpawnsForm entities.MobSpawns
+	if err := c.ShouldBindBodyWithJSON(&mobSpawnsForm); err != nil {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	// check if creature exist
+	creature, creatureErr := repository.GetCreatureByID(ctx, db, id)
+	if creatureErr != nil {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	// check if location exist
+	location, locationErr := repository.GetLocationByID(ctx, db, strconv.Itoa(mobSpawnsForm.LocationID))
+	if locationErr != nil {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+	// check if location do not already assign
+	var mobSpawns entities.MobSpawns
+	err := pgxscan.Get(ctx, db, &mobSpawns, `SELECT * FROM mob_spawn WHERE mob_id = $1 AND location_id = $2`, creature.ID, location.ID)
+	if errors.Is(pgx.ErrNoRows, err) {
+		_, insertErr := db.Exec(ctx, `INSERT INTO mob_spawn (mob_id, location_id, level_required, spawn_rate) values ($1,$2,$3,$4)`,
+			creature.ID, location.ID, mobSpawnsForm.LevelRequired, mobSpawnsForm.SpawnRate)
+		if insertErr != nil {
+			c.JSON(http.StatusBadRequest, "bad request")
+			return
+		}
+
+		c.JSON(http.StatusOK, &mobSpawns)
+		c.Done()
+	}
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	if mobSpawns.MobID == 0 {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+}
+func UpdateMobSkill(c *gin.Context) {
+	db := database.Connect()
+	id := c.Param("id")
+
+	var mobSkillForm entities.MobSkill
+	if err := c.ShouldBindBodyWithJSON(&mobSkillForm); err != nil {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	// check if creature exist
+	creature, creatureErr := repository.GetCreatureByID(ctx, db, id)
+	if creatureErr != nil {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	// check if skill exist
+	skill, skillErr := repository.GetSkillByID(ctx, db, strconv.Itoa(mobSkillForm.SkillID))
+	if skillErr != nil {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	// check if creature do not already have this skill
+	var mobSkill entities.MobSkill
+	err := pgxscan.Get(ctx, db, &mobSkill, `SELECT * FROM mob_skill WHERE mob_id = $1 AND skill_id = $2`, creature.ID, skill.ID)
+	if errors.Is(pgx.ErrNoRows, err) {
+		_, insertErr := db.Exec(ctx, `INSERT INTO mob_skill (mob_id, skill_id) values ($1,$2)`,
+			creature.ID, skill.ID)
+		if insertErr != nil {
+			c.JSON(http.StatusBadRequest, "bad request")
+			return
+		}
+
+		c.JSON(http.StatusOK, &mobSkill)
+		c.Done()
+	}
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+}
+func UpdateMobLoot(c *gin.Context) {}
+
+// DELETE \\
+
+func DeleteMob(c *gin.Context) {
+	db := database.Connect()
+	id := c.Param("id")
+
+	// check if mob exist
+	mob, err := repository.GetCreatureByID(ctx, db, id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+	// delete mob skill
+	_, deleteErr := db.Exec(ctx, `DELETE FROM mob_skill WHERE mob_id = $1`, mob.ID)
+	if deleteErr != nil {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+	// delete mob spawn
+	_, deleteErr = db.Exec(ctx, `DELETE FROM mob_spawn WHERE mob_id = $1`, mob.ID)
+	if deleteErr != nil {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	if mob.IsPet {
+		// select pet
+		var selectedPets entities.PetsMounts
+		err := pgxscan.Get(ctx, db, &selectedPets, `SELECT * FROM pets_mounts where mob_id = $1`, mob.ID)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, "bad request")
+			return
+		}
+		// delete pets to all user
+		_, deleteErr = db.Exec(ctx, `DELETE FROM player_pets_mounts where pet_id = $1`, selectedPets.ID)
+		if deleteErr != nil {
+			c.JSON(http.StatusBadRequest, "bad request")
+			return
+		}
+		// delete pet
+		_, deleteErr = db.Exec(ctx, `DELETE FROM pets_mounts where mob_id = $1`, mob.ID)
+		if deleteErr != nil {
+			c.JSON(http.StatusBadRequest, "bad request")
+			return
+		}
+	}
+	// delete mob
+	_, deleteErr = db.Exec(ctx, `DELETE FROM mobs where id = $1`, mob.ID)
+	if deleteErr != nil {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	c.Status(http.StatusOK)
+	c.Done()
+}
+func DeleteMobSpawn(c *gin.Context) {
+	db := database.Connect()
+	id := c.Param("id")
+
+	var mobSpawnForm int
+	if err := c.ShouldBindBodyWithJSON(&mobSpawnForm); err != nil {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	// check if mob exist
+	mob, err := repository.GetCreatureByID(ctx, db, id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	// delete mob spawn
+	_, deleteErr := db.Exec(ctx, `DELETE FROM mob_spawn WHERE mob_id = $1 and location_id = $2`, mob.ID, mobSpawnForm)
+	if deleteErr != nil {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	c.Status(http.StatusOK)
+	c.Done()
+}
+func DeleteMobSkill(c *gin.Context) {
+	db := database.Connect()
+	id := c.Param("id")
+
+	var creatureSkillForm int
+	if err := c.ShouldBindBodyWithJSON(&creatureSkillForm); err != nil {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	// check if creature exist
+	creature, err := repository.GetCreatureByID(ctx, db, id)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	// delete creature spawn
+	_, deleteErr := db.Exec(ctx, `DELETE FROM mob_skill WHERE mob_id = $1 and skill_id = $2`, creature.ID, creatureSkillForm)
+	if deleteErr != nil {
+		c.JSON(http.StatusBadRequest, "bad request")
+		return
+	}
+
+	c.Status(http.StatusOK)
+	c.Done()
+}
+func DeleteMobLoot(c *gin.Context) {}
--- core/entities/entities.go
+++ core/entities/entities.go
@@ -2,64 +2,18 @@ package entities
 
 import "time"
 
-type Player struct {
-	ID            int    `json:"id"`
-	Email         string `json:"email"`
-	Username      string `json:"username"`
-	RaceID        int    `json:"race_id"`
-	JobID         int    `json:"job_id"`
-	Exp           int    `json:"exp"`
-	Po            int    `json:"po"`
-	Level         int    `json:"level"`
-	GuildID       int    `json:"guild_id"` // 0 = no guild
-	InventorySize int    `json:"inventorySize"`
-	LocationId    int    `json:"locationId"`
-}
-
-type Inventory struct {
-	PlayerID int `json:"player_id"`
-	ItemID   int `json:"item_id"`
-	Quantity int `json:"quantity"`
-}
-
-type Job struct {
-	ID           int    `json:"id"`
-	Name         string `json:"name"`
-	Description  string `json:"description"` // description of the job
-	Strength     int    `json:"strength"`
-	Constitution int    `json:"constitution"`
-	Dexterity    int    `json:"dexterity"`
-	Intelligence int    `json:"intelligence"`
-	Mana         int    `json:"mana"`
-	Stamina      int    `json:"stamina"`
-	Wisdom       int    `json:"wisdom"`
-	Charisma     int    `json:"charisma"`
-}
-
-type Race struct {
-	ID          int    `json:"id"`
-	Name        string `json:"name"`
-	Description string `json:"description"` // description of the race
-	Mana        int    `json:"mana"`
-	Stamina     int    `json:"stamina"`
-	Wisdom      int    `json:"wisdom"`
-	Charisma    int    `json:"charisma"`
-}
-
-type Items struct {
-	ID               int    `json:"id"`
-	Name             string `json:"name"`
-	Description      string `json:"description"`
-	Type             int    `json:"type"` // 0 = Equipable, 1 = Consomable, 2 = Quest, 3 = Resources
-	Strength         int    `json:"strength"`
-	Constitution     int    `json:"constitution"`
-	Mana             int    `json:"mana"`
-	Stamina          int    `json:"stamina"`
-	Dexterity        int    `json:"dexterity"`
-	Intelligence     int    `json:"intelligence"`
-	Wisdom           int    `json:"wisdom"`
-	Charisma         int    `json:"charisma"`
-	EnchantmentLevel int    `json:"enchantmentLevel"`
+type Equipment struct {
+	PlayerId       int `json:"player_id"`
+	Helmet         int `json:"helmet"`
+	Chestplate     int `json:"chestplate"`
+	Leggings       int `json:"leggings"`
+	Boots          int `json:"boots"`
+	Mainhand       int `json:"mainhand"`
+	Offhand        int `json:"offhand"`
+	AccessorySlot0 int `json:"accessory_slot_0" db:"accessory_slot_0"`
+	AccessorySlot1 int `json:"accessory_slot_1" db:"accessory_slot_1"`
+	AccessorySlot2 int `json:"accessory_slot_2" db:"accessory_slot_2"`
+	AccessorySlot3 int `json:"accessory_slot_3" db:"accessory_slot_3"`
 }
 
 type Guild struct {
@@ -75,19 +29,39 @@ type GuildMembers struct {
 	PlayerID int `json:"player_id"`
 }
 
-type Skill struct {
-	ID           int    `json:"id"`
-	Name         string `json:"name"`
-	Type         string `json:"type"` // physic or magic
-	Description  string `json:"description"`
-	Strength     int    `json:"strength"`
-	Constitution int    `json:"constitution"`
-	Mana         int    `json:"mana"`
-	Stamina      int    `json:"stamina"`
-	Dexterity    int    `json:"dexterity"`
-	Intelligence int    `json:"intelligence"`
-	Wisdom       int    `json:"wisdom"`
-	Charisma     int    `json:"charisma"`
+type HuntAction struct {
+	PlayerID   int       `json:"playerID"`
+	LocationID int       `json:"locationID"`
+	MobID      int       `json:"mobID"`
+	StartAt    time.Time `json:"startAt"`
+	EndAt      time.Time `json:"endAt"`
+}
+
+type Inventory struct {
+	PlayerID int `json:"player_id"`
+	ItemID   int `json:"item_id"`
+	Quantity int `json:"quantity"`
+}
+
+type Items struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Type        int    `json:"type"` // 0 = Equipable, 1 = Consomable, 2 = Quest, 3 = Resources
+
+}
+
+type ItemStat struct {
+	ItemID           int
+	Strength         int `json:"strength"`
+	Constitution     int `json:"constitution"`
+	Mana             int `json:"mana"`
+	Stamina          int `json:"stamina"`
+	Dexterity        int `json:"dexterity"`
+	Intelligence     int `json:"intelligence"`
+	Wisdom           int `json:"wisdom"`
+	Charisma         int `json:"charisma"`
+	EnchantmentLevel int `json:"enchantmentLevel"`
 }
 
 type JobSkill struct {
@@ -106,71 +80,50 @@ type JobSkill struct {
 	Charisma     int    `json:"charisma"`
 }
 
-type UserSkill struct {
-	PlayerId int
-	SkillID  int
-}
-
-type UserJobSkill struct {
-	PlayerId int
-	SkillID  int
-}
-
-type CreatureSkill struct {
-	CreatureId int
-	SkillID    int
-}
-
-type UserPet struct {
-	PetID    int `json:"pet_id"`
-	PlayerId int `json:"user_id"`
-}
-type SummonBeast struct {
+type Job struct {
 	ID           int    `json:"id"`
-	PlayerId     int    `json:"user_id"`
 	Name         string `json:"name"`
+	Description  string `json:"description"` // description of the job
 	Strength     int    `json:"strength"`
 	Constitution int    `json:"constitution"`
-	Mana         int    `json:"mana"`
-	Stamina      int    `json:"stamina"`
 	Dexterity    int    `json:"dexterity"`
 	Intelligence int    `json:"intelligence"`
+	Mana         int    `json:"mana"`
+	Stamina      int    `json:"stamina"`
 	Wisdom       int    `json:"wisdom"`
 	Charisma     int    `json:"charisma"`
 }
-type Stats struct {
-	PlayerId     int `json:"user_id"`
-	HP           int `json:"HP"`
-	Strength     int `json:"strength"`
-	Constitution int `json:"constitution"`
-	Mana         int `json:"mana"`
-	Stamina      int `json:"stamina"`
-	Dexterity    int `json:"dexterity"`
-	Intelligence int `json:"intelligence"`
-	Charisma     int `json:"charisma"`
-	Wisdom       int `json:"wisdom"`
-}
-type PetsMounts struct {
-	ID          int  `json:"id"`
-	CreatureID  int  `json:"creature_id"`
-	IsMountable bool `json:"is_mountable"`
-	Speed       int  `json:"speed"` // 0 = slow, 1 = normal, 2 = fast
-}
-type Equipment struct {
-	PlayerId       int `json:"player_id"`
-	Helmet         int `json:"helmet"`
-	Chestplate     int `json:"chestplate"`
-	Leggings       int `json:"leggings"`
-	Boots          int `json:"boots"`
-	Mainhand       int `json:"mainhand"`
-	Offhand        int `json:"offhand"`
-	AccessorySlot0 int `json:"accessory_slot_0" db:"accessory_slot_0"`
-	AccessorySlot1 int `json:"accessory_slot_1" db:"accessory_slot_1"`
-	AccessorySlot2 int `json:"accessory_slot_2" db:"accessory_slot_2"`
-	AccessorySlot3 int `json:"accessory_slot_3" db:"accessory_slot_3"`
+
+type Locations struct {
+	ID         int    `json:"id"`
+	Name       string `json:"name"`
+	IsSafety   bool   `json:"isSafety"`
+	Difficulty int    `json:"difficulty"`
+	Type       int    `json:"type"` // 0 = Resource, 1 = Mobs, 2 = City
+	Size       int    `json:"size"`
 }
 
-type Creatures struct {
+type Loot struct {
+	ID          int
+	MobID       int
+	ItemID      int
+	QuantityMax int
+	Rarity      int
+}
+
+type MobSkill struct {
+	MobId   int
+	SkillID int
+}
+
+type MobSpawns struct {
+	MobID         int `json:"creatureID"`
+	LocationID    int `json:"emplacementID"`
+	LevelRequired int `json:"levelRequired"`
+	SpawnRate     int `json:"spawnRate"`
+}
+
+type Mob struct {
 	ID           int    `json:"id"`
 	Name         string `json:"name"`
 	IsPet        bool   `json:"is_pet"`
@@ -186,11 +139,48 @@ type Creatures struct {
 	Wisdom       int    `json:"wisdom"`
 }
 
-type CreatureSpawns struct {
-	CreatureID    int `json:"creatureID"`
-	EmplacementID int `json:"emplacementID"`
-	LevelRequired int `json:"levelRequired"`
-	SpawnRate     int `json:"spawnRate"`
+type PetsMounts struct {
+	ID          int  `json:"id"`
+	MobID       int  `json:"creature_id"`
+	IsMountable bool `json:"is_mountable"`
+	Speed       int  `json:"speed"` // 0 = slow, 1 = normal, 2 = fast
+}
+
+type PlayerJobSkill struct {
+	PlayerId int
+	SkillID  int
+}
+
+type PlayerPet struct {
+	PetID    int `json:"pet_id"`
+	PlayerId int `json:"user_id"`
+}
+
+type PlayerSkill struct {
+	PlayerId int
+	SkillID  int
+}
+
+type Player struct {
+	ID            int    `json:"id"`
+	UserID        int    `json:"userID"`
+	Email         string `json:"email"`
+	Username      string `json:"username"`
+	RaceID        int    `json:"race_id"`
+	JobID         int    `json:"job_id"`
+	Exp           int    `json:"exp"`
+	Po            int    `json:"po"`
+	Level         int    `json:"level"`
+	GuildID       int    `json:"guild_id"` // 0 = no guild
+	InventorySize int    `json:"inventorySize"`
+	LocationId    int    `json:"locationId"`
+}
+
+type PlayerAction struct {
+	PlayerId  int       `json:"user_id"`
+	Action    string    `json:"action"`
+	CreatedAt time.Time `json:"created_at"`
+	EndAt     time.Time `json:"end_at"`
 }
 
 type Quests struct {
@@ -215,13 +205,14 @@ type Objectives struct {
 	MaxObjective int    `json:"max_objective"` // {"max_track": 10}
 }
 
-type Locations struct {
-	ID         int    `json:"id"`
-	Name       string `json:"name"`
-	Type       int    `json:"type"` // 0 = Resource, 1 = Mobs, 2 = City
-	IsSafety   bool   `json:"isSafety"`
-	Difficulty int    `json:"difficulty"`
-	Size       int    `json:"size"`
+type Race struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"` // description of the race
+	Mana        int    `json:"mana"`
+	Stamina     int    `json:"stamina"`
+	Wisdom      int    `json:"wisdom"`
+	Charisma    int    `json:"charisma"`
 }
 
 type Resources struct {
@@ -237,21 +228,48 @@ type ResourcesType struct {
 	Name string `json:"name"`
 }
 
-type PlayerAction struct {
-	PlayerId  int       `json:"user_id"`
-	Action    string    `json:"action"`
-	CreatedAt time.Time `json:"created_at"`
-	EndAt     time.Time `json:"end_at"`
+type Skill struct {
+	ID          int    `json:"id"`
+	Name        string `json:"name"`
+	Type        string `json:"type"` // physic or magic
+	Description string `json:"description"`
 }
 
-type FightOrder struct {
-	Name string
-	ID   int
+type SkillStat struct {
+	SkillID      int
+	Strength     int `json:"strength"`
+	Constitution int `json:"constitution"`
+	Mana         int `json:"mana"`
+	Stamina      int `json:"stamina"`
+	Dexterity    int `json:"dexterity"`
+	Intelligence int `json:"intelligence"`
+	Wisdom       int `json:"wisdom"`
+	Charisma     int `json:"charisma"`
 }
 
-type HuntAction struct {
-	PlayerID  int
-	BtnID     string
-	MessageID string
-	ChannelID string
+type Stats struct {
+	PlayerId     int `json:"user_id"`
+	HP           int `json:"HP"`
+	Strength     int `json:"strength"`
+	Constitution int `json:"constitution"`
+	Mana         int `json:"mana"`
+	Stamina      int `json:"stamina"`
+	Dexterity    int `json:"dexterity"`
+	Intelligence int `json:"intelligence"`
+	Charisma     int `json:"charisma"`
+	Wisdom       int `json:"wisdom"`
+}
+
+type SummonBeast struct {
+	ID           int    `json:"id"`
+	PlayerId     int    `json:"user_id"`
+	Name         string `json:"name"`
+	Strength     int    `json:"strength"`
+	Constitution int    `json:"constitution"`
+	Mana         int    `json:"mana"`
+	Stamina      int    `json:"stamina"`
+	Dexterity    int    `json:"dexterity"`
+	Intelligence int    `json:"intelligence"`
+	Wisdom       int    `json:"wisdom"`
+	Charisma     int    `json:"charisma"`
 }
